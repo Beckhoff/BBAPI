3f78743ceb57192bbcc6156ffe0c5add92d7005e
Patrick Bruenn <p.bruenn@beckhoff.com>
2018-05-22 12:05:36 +0200

    tools: use fructose-1.4.0

--- tools/10_get_fructose.sh
+++ tools/10_get_fructose.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 SCRIPT_PATH="$( cd "$( dirname "$0" )" && pwd )"
-NAME=fructose-1.3.0
+NAME=fructose-1.4.0
+FILENAME=${NAME}.tar.gz
 
 pushd ${SCRIPT_PATH}
 
 if ! sha512sum -c ${NAME}.sha512; then
-	wget https://downloads.sourceforge.net/project/fructose/fructose/${NAME}/${NAME}.tgz
+	wget https://downloads.sourceforge.net/project/fructose/fructose/${NAME}/${FILENAME}
 fi
 
-tar xf ${NAME}.tgz
+tar xf ${FILENAME}
 mv ${NAME}/fructose/include/fructose/ ${SCRIPT_PATH}/
 rm -rf ${NAME}/
