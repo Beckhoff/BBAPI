fd4d8cac5a2688a84414245d10ada97445fcf566
Patrick Bruenn <p.bruenn@beckhoff.com>
2021-04-22 12:12:15 +0200

    ci: publish debian package only from master
    Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

--- .ci/publish_debian_package.sh
+++ .ci/publish_debian_package.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2020 Beckhoff Automation GmbH & Co. KG
+# Copyright (C) 2020 - 2021 Beckhoff Automation GmbH & Co. KG
 
 set -e
 set -u
 
-if test -z "${SSH_AUTH_SOCK-}"; then
-	eval $(ssh-agent -s)
-	echo "${APTLY_SSH_KEY}" | ssh-add -
-fi
-
+# release on master only
 if ! bdpg git-pre-push-check master; then
 	exit 0
+fi
+
+if test -z "${SSH_AUTH_SOCK-}"; then
+	eval $(ssh-agent -s)
+	ssh-add - < "${SSH_KEY_APTLY}"
 fi
 
 readonly package_dir="$(cd "$(dirname "${0}")" && pwd)/.."
