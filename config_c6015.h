Beckhoff/BBAPI

commit 9d01f4ffe704b50b21795ef5730e59bd44b9e3f4

unittest: define TEST_DEVICE within specific headers

config_c6015.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2015 - 2018 Beckhoff Automation GmbH &Co. KG
+    Copyright (C) 2015 - 2019 Beckhoff Automation GmbH &Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
-#ifndef TEST_DEVICE
-#define TEST_DEVICE DEVICE_C6015
+#ifdef TEST_DEVICE
+#define TEST_DEVICE 0xC6015
 
 /** general configuration */
 #define CONFIG_GENERAL_BOARDINFO {"CB3163\0", 5, 0, 118}

config_cx2030_cx2100-0004.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2014 - 2018 Beckhoff Automation GmbH & Co. KG
+    Copyright (C) 2014 - 2019 Beckhoff Automation GmbH & Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
 #ifndef TEST_DEVICE
-#define TEST_DEVICE DEVICE_CX2030_CX2100_0004
+#define TEST_DEVICE 0x21000004
 
 /** general configuration */
 #define CONFIG_GENERAL_BOARDINFO {"CX20x0\0", 1, 1, 52}

config_cx2030_cx2100-0904.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2014 - 2018 Beckhoff Automation GmbH & Co. KG
+    Copyright (C) 2014 - 2019 Beckhoff Automation GmbH & Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
 #ifndef TEST_DEVICE
-#define TEST_DEVICE DEVICE_CX2030_CX2100_0904
+#define TEST_DEVICE 0x21000904
 
 /** general configuration */
 #define CONFIG_GENERAL_BOARDINFO {"CX20x0\0", 2, 1, 100}

config_cx5000.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2018 Beckhoff Automation GmbH & Co. KG
+    Copyright (C) 2018 - 2019 Beckhoff Automation GmbH & Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
 #ifndef TEST_DEVICE
-#define TEST_DEVICE DEVICE_CX5000
+#define TEST_DEVICE 0x5000
 
 /** general configuration */
 #define CONFIG_GENERAL_BOARDINFO {"CX553\0", 1, 0, 128}

config_cx5130.h
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2015 - 2018 Beckhoff Automation GmbH &Co. KG
+    Copyright (C) 2015 - 2019 Beckhoff Automation GmbH &Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
 #ifndef TEST_DEVICE
-#define TEST_DEVICE DEVICE_CX5130
+#define TEST_DEVICE 0x5130
 
 /** general configuration */
 #define CONFIG_GENERAL_BOARDINFO {"CX51x0\0", 1, 0, 81}

test_config.h
@@ -1,22 +1,14 @@
 // SPDX-License-Identifier: MIT
 /**
     Beckhoff BIOS API driver to access hwmon sensors for Beckhoff IPCs
-    Copyright (C) 2014 - 2018 Beckhoff Automation GmbH & Co. KG
+    Copyright (C) 2014 - 2019 Beckhoff Automation GmbH & Co. KG
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
-#ifndef _TEST_CONFIG_H_
-#define _TEST_CONFIG_H_
+#pragma once
 
 /** test mode selection */
 #define CONFIG_INTERACTIVE 1
-
-/** device definitions */
-#define DEVICE_C6015 0xC6015
-#define DEVICE_CX2030_CX2100_0004 0x21000004
-#define DEVICE_CX2030_CX2100_0904 0x21000904
-#define DEVICE_CX5000 0x5000
-#define DEVICE_CX5130 0x5130
 
 /** reused ranges */
 #define BOOTCOUNTER_RANGE 1, 15000
@@ -95,5 +87,3 @@
 
 /** Watchdog configuration */
 #define CONFIG_WATCHDOG_OPTIONS WDIOF_SETTIMEOUT | WDIOF_MAGICCLOSE | WDIOF_KEEPALIVEPING
-
-#endif /* #ifndef _TEST_CONFIG_H_ */

commit 3cbb35b1fb3dd0643f9840e0c7ce04bf064db0c0

unittest: fix bug in config_c6015.h
It seems we introduced a bug in Commit
9d01f4ffe704b50b21795ef5730e59bd44b9e3f4.

It makes no sence to have ifndef TEST_DEVICE.

Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

config_c6015.h
@@ -5,7 +5,7 @@
     Author: Patrick Bruenn <p.bruenn@beckhoff.com>
 */
 
-#ifdef TEST_DEVICE
+#ifndef TEST_DEVICE
 #define TEST_DEVICE 0xC6015
 
 /** general configuration */